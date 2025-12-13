f64b82e44181f3523b605f867630752342f65a91
nehaghauri <nehasaleem596@gmail.com>
2025-12-13 21:54:25 +0500

    fix:  Clean up EKS module files

--- infra/terraform/modules/eks/outputs.tf
+++ infra/terraform/modules/eks/outputs.tf
@@ -1,22 +1,16 @@
 output "cluster_id" {
-  description = "EKS cluster ID"
-  value       = aws_eks_cluster. main.id
+  description = "The name/id of the EKS cluster"
+  value       = aws_eks_cluster.main.id
 }
 
-output "cluster_name" {
-  description = "EKS cluster name"
-  value       = aws_eks_cluster. main.name
+output "cluster_arn" {
+  description = "The Amazon Resource Name (ARN) of the cluster"
+  value       = aws_eks_cluster.main. arn
 }
 
 output "cluster_endpoint" {
-  description = "EKS cluster endpoint"
-  value       = aws_eks_cluster. main.endpoint
-  sensitive   = true
-}
-
-output "cluster_version" {
-  description = "EKS cluster version"
-  value       = aws_eks_cluster. main.version
+  description = "Endpoint for EKS control plane"
+  value       = aws_eks_cluster.main.endpoint
 }
 
 output "cluster_security_group_id" {
@@ -24,9 +18,9 @@ output "cluster_security_group_id" {
   value       = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
 }
 
-output "cluster_iam_role_arn" {
-  description = "IAM role ARN of the EKS cluster"
-  value       = aws_iam_role.cluster.arn
+output "cluster_certificate_authority_data" {
+  description = "Base64 encoded certificate data required to communicate with the cluster"
+  value       = aws_eks_cluster.main.certificate_authority[0].data
 }
 
 output "node_group_id" {
@@ -34,7 +28,12 @@ output "node_group_id" {
   value       = aws_eks_node_group.main.id
 }
 
-output "oidc_provider_arn" {
-  description = "OIDC provider ARN"
-  value       = aws_iam_openid_connect_provider.cluster.arn
+output "node_group_arn" {
+  description = "Amazon Resource Name (ARN) of the EKS Node Group"
+  value       = aws_eks_node_group.main.arn
+}
+
+output "node_group_status" {
+  description = "Status of the EKS node group"
+  value       = aws_eks_node_group.main.status
 }
