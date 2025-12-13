508ab79be6253712f6721281f84466455f35aa8e
nehaghauri <nehasaleem596@gmail.com>
2025-12-13 21:49:00 +0500

    fix:  Add missing EKS module variables and fix outputs

--- infra/terraform/modules/eks/variables.tf
+++ infra/terraform/modules/eks/variables.tf
@@ -1,43 +1,82 @@
-variable "project_name" {
-  description = "Project name"
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
   type        = string
 }
 
-variable "environment" {
-  description = "Environment name"
-  type        = string
-}
-
-variable "vpc_id" {
-  description = "VPC ID"
-  type        = string
-}
-
-variable "private_subnets" {
-  description = "Private subnet IDs"
+variable "subnet_ids" {
+  description = "List of subnet IDs for the EKS cluster"
   type        = list(string)
-}
-
-variable "node_instance_types" {
-  description = "EC2 instance types for nodes"
-  type        = list(string)
-  default     = ["t3.small"]
 }
 
 variable "desired_size" {
-  description = "Desired number of nodes"
+  description = "Desired number of worker nodes"
   type        = number
   default     = 2
 }
 
+variable "max_size" {
+  description = "Maximum number of worker nodes"
+  type        = number
+  default     = 3
+}
+
 variable "min_size" {
-  description = "Minimum number of nodes"
+  description = "Minimum number of worker nodes"
   type        = number
   default     = 1
 }
 
-variable "max_size" {
-  description = "Maximum number of nodes"
-  type        = number
-  default     = 3
+variable "instance_types" {
+  description = "List of instance types for the node group"
+  type        = list(string)
+  default     = ["t3.medium"]
+}
+
+variable "tags" {
+  description = "Tags to apply to resources"
+  type        = map(string)
+  default     = {}
+}
+EOF
+
+# Fix EKS outputs.tf (remove OIDC provider reference)
+cat > infra/terraform/modules/eks/outputs.tf <<'EOF'
+output "cluster_id" {
+  description = "The name/id of the EKS cluster"
+  value       = aws_eks_cluster.main.id
+}
+
+output "cluster_arn" {
+  description = "The Amazon Resource Name (ARN) of the cluster"
+  value       = aws_eks_cluster.main.arn
+}
+
+output "cluster_endpoint" {
+  description = "Endpoint for EKS control plane"
+  value       = aws_eks_cluster.main.endpoint
+}
+
+output "cluster_security_group_id" {
+  description = "Security group ID attached to the EKS cluster"
+  value       = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
+}
+
+output "cluster_certificate_authority_data" {
+  description = "Base64 encoded certificate data required to communicate with the cluster"
+  value       = aws_eks_cluster.main.certificate_authority[0].data
+}
+
+output "node_group_id" {
+  description = "EKS node group ID"
+  value       = aws_eks_node_group.main.id
+}
+
+output "node_group_arn" {
+  description = "Amazon Resource Name (ARN) of the EKS Node Group"
+  value       = aws_eks_node_group.main.arn
+}
+
+output "node_group_status" {
+  description = "Status of the EKS node group"
+  value       = aws_eks_node_group.main.status
 }
