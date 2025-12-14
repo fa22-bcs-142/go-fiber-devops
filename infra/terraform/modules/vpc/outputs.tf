69eb789a5d0f43c7faa6c1076a70ea9148966f50
nehaghauri <nehasaleem596@gmail.com>
2025-12-14 12:45:50 +0500

    test

--- infra/terraform/modules/vpc/outputs.tf
+++ infra/terraform/modules/vpc/outputs.tf
@@ -1,34 +1,14 @@
 output "vpc_id" {
-  description = "VPC ID"
-  value       = aws_vpc.main.id
-}
-
-output "vpc_cidr" {
-  description = "VPC CIDR block"
-  value       = aws_vpc. main.cidr_block
+  description = "Simulated VPC id (for validation)"
+  value       = local.vpc_id
 }
 
 output "public_subnets" {
-  description = "Public subnet IDs"
-  value       = aws_subnet.public[*].id
+  description = "List of simulated public subnet ids"
+  value       = local.public_subnets
 }
 
 output "private_subnets" {
-  description = "Private subnet IDs"
-  value       = aws_subnet.private[*].id
-}
-
-output "app_security_group_id" {
-  description = "Application security group ID"
-  value       = aws_security_group.app.id
-}
-
-output "db_security_group_id" {
-  description = "Database security group ID"
-  value       = aws_security_group.db.id
-}
-
-output "nat_gateway_id" {
-  description = "NAT Gateway ID"
-  value       = aws_nat_gateway.main.id
+  description = "List of simulated private subnet ids"
+  value       = local.private_subnets
 }
--- infra/terraform/modules/vpc/variables.tf
+++ infra/terraform/modules/vpc/variables.tf
@@ -1,19 +1,41 @@
-variable "project_name" {
-  description = "Project name"
-  type        = string
-}
-
-variable "environment" {
-  description = "Environment name"
-  type        = string
-}
-
 variable "vpc_cidr" {
-  description = "CIDR block for VPC"
+  description = "CIDR for the VPC"
   type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "availability_zones" {
-  description = "Availability zones"
+  description = "List of AZs"
   type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = []
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = []
+}
+
+variable "project_name" {
+  description = "Project name for resource naming"
+  type        = string
+  default     = ""
+}
+
+variable "environment" {
+  description = "Environment (dev/prod)"
+  type        = string
+  default     = ""
+}
+
+variable "tags" {
+  description = "Optional tags map"
+  type        = map(string)
+  default     = {}
 }
