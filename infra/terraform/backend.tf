e80eacb4c0c0074e3ede6309209bd8af71e71841
fatima <fatimashahzad911@gmail.com>
2025-12-16 01:50:05 +0500

    Configure Terraform backend and provider for AWS deployment

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "s3" {
-    bucket         = "nehaghauri-go-fiber-terraform-state"
-    key            = "prod/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "go-fiber-terraform-lock"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket         = "nehaghauri-go-fiber-terraform-state"
+#     key            = "terraform.tfstate"
+#     region         = "ap-south-1"
+#     encrypt        = true
+#     dynamodb_table = "go-fiber-terraform-lock"
+#   }
+# }
